053504006662f4ca4b072f7f4904b818d124d133
Michlvb <Michael.c.lvb@gmail.com>
2021-06-01 16:06:55 +0200

    Added department picker

--- components/login page/Login.js
+++ components/login page/Login.js
@@ -2,20 +2,39 @@ import React, {useState} from 'react';
 import { SafeAreaView, StyleSheet, View, StatusBar, TextInput, Text, Button} from "react-native";
 import {AddUser} from '../firebase/firebase.utils'
 import storeData from '../localstorage/LocalStorage'
+import {Picker} from '@react-native-picker/picker'
+
 
 export default function Login(props){
     
-    let [name, setName] = useState("");
-    let [department, setDepartment] = useState("");
+    const [name, setName] = useState("");
+    const [department, setDepartment] = useState("");
+    const departments = ["Anatomie", "Anesthesiologie, Pijn en Palliatieve Geneeskunde", "Apotheek", "Cardiologie", 
+                        "Cardio-thoracale Chirurgie", "Chirurgische Dagbehandeling", "Cognitive Neuroscience",
+                        "Dermatologie", "Eerstelijnsgeneeskunde", "Fysiologie", "Fysiotherapie", "Geestelijke verzorging en Pastoraat",
+                        "Genetica", "Geriatrie", "Health Evidence", "Heelkunde", "Hematologie", "Intensive Care", "Interne Geneeskunde",
+                        "IQ healthcare", "Keel-Neus-Oorheelkunde", "Laboratorium-geneeskunde", "Longziekten", "Maag-, Darm- en Leverziekten",
+                        "Medische Microbiologie", "Medische Oncologie", "Medische Psychologie", "Mobiel Medisch Team", "Mond-Kaak-Aangezichts-chirurgie",
+                        "Mortuarium", "Neonatologie", "Neurochirurgie", "Neurologie", "Nierziekten", "Oogheelkunde", "Operatiekamers", "Orthopedie", "Pathologie",
+                        "Plastische Chirurgie", "Psychiatrie", "Radiologie en Nucleaire Geneeskunde", "Radiotherapie", "Reumatische Ziekten", "Revalidatie", "Spoedeisende Hulp",
+                        "Tandheelkunde", "Urologie", "Verloskunde en Gynaecologie"]
 
-
+                        
     return (
+        
         <SafeAreaView style={styles.container}>
             <View style={styles.mainPage}>
                 <Text style={styles.text}>Gebruikersnaam</Text>
                 <TextInput style={styles.input} onChangeText={(text) => setName(text.toLowerCase())} ></TextInput>
                 <Text style={styles.text}>Afdeling</Text>
-                <TextInput style={styles.input} onChangeText={(text) => setDepartment(text.toLowerCase())}></TextInput>
+                <Picker
+                selectedValue={department}
+                onValueChange={(item) => setDepartment(item)}
+                >
+                {departments.map((val, index) => {
+                    return <Picker.Item label={val} key={index} value={val}/>
+                })}
+                </Picker>
                 <Button 
                 title="Submit" 
                 onPress={() => 
